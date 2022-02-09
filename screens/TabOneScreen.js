1bd3544560677b80c4d47f7182b80ae3e8c2cf68
Maksym Kazhaiev <mkazhaev746@gmail.com>
2022-02-09 13:32:03 +0200

    first commit

---
+++ screens/TabOneScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Log in</Text>
+      <TextInput
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Enter your phone number"
+        keyboardType="numeric"
+        maxLength={9}
+        textAlign={'center'}
+      />
+      <TextInput
+        // style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
